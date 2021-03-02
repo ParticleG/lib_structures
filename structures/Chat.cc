7093624882e7cd03b9c4226387bd81d2af628c0a
Particle_G <particle_g@outlook.com>
2021-03-03 00:21:31 +0800

    - Implementation of Chat structure

---
+++ structures/Chat.cc
@@ -0,0 +1,22 @@
+//
+// Created by Parti on 2021/2/23.
+//
+
+#include <drogon/drogon.h>
+#include <structures/Chat.h>
+
+using namespace drogon;
+using namespace drogon_model;
+using namespace tech::structures;
+using namespace std;
+
+Chat::Chat(const int64_t &id) {
+    Mapper<Techmino::Info> infoMapper(app().getDbClient());
+    auto info = infoMapper.findOne(Criteria(Techmino::Info::Cols::__id, CompareOperator::EQ, id));
+    _info = make_shared<Techmino::Info>(info);
+}
+
+shared_ptr<Techmino::Info> Chat::getInfo() const {
+    return _info;
+}
+
---
+++ structures/Chat.h
@@ -0,0 +1,21 @@
+//
+// Created by Parti on 2021/2/23.
+//
+
+#pragma once
+
+#include <models/Info.h>
+
+namespace tech::structures {
+    class Chat {
+    public:
+        Chat() = default;
+
+        Chat(const int64_t &id);
+
+        std::shared_ptr<drogon_model::Techmino::Info> getInfo() const;
+
+    private:
+        std::shared_ptr<drogon_model::Techmino::Info> _info{};
+    };
+}
