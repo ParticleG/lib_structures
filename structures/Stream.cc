dbca2c9f56813564b42d0ed7d14c9b9d6142b97f
Particle_G <particle_g@outlook.com>
2021-03-06 05:42:58 +0800

    - Implemented Stream structure.

---
+++ structures/Stream.cc
@@ -0,0 +1,29 @@
+//
+// Created by Parti on 2021/2/23.
+//
+
+#include <structures/Stream.h>
+
+using namespace drogon;
+using namespace tech::structures;
+using namespace std;
+
+Stream::Stream(const int &uid) : BasePlayer(true), _uid(uid) {}
+
+int Stream::getUid() const { return _uid; }
+
+uint64_t Stream::getScore() const { return _score; }
+
+void Stream::setScore(const uint64_t &score) { _score = score; }
+
+uint64_t Stream::getSurvivalTime() const { return _survivalTime; }
+
+void Stream::setSurvivalTime(const uint64_t &survivalTime) { _survivalTime = survivalTime; }
+
+uint64_t Stream::getPlace() const { return _place; }
+
+void Stream::setPlace(const uint64_t &place) { _place = place; }
+
+bool Stream::getDead() const { return _dead; }
+
+void Stream::setDead(const bool &dead) { _dead = dead; }
---
+++ structures/Stream.h
@@ -0,0 +1,37 @@
+//
+// Created by Parti on 2021/2/23.
+//
+
+#pragma once
+
+#include <structures/BasePlayer.h>
+
+namespace tech::structures {
+    class Stream : public BasePlayer {
+    public:
+        explicit Stream(const int &uid);
+
+        int getUid() const;
+
+        uint64_t getScore() const;
+
+        void setScore(const uint64_t &score);
+
+        uint64_t getSurvivalTime() const;
+
+        void setSurvivalTime(const uint64_t &survivalTime);
+
+        uint64_t getPlace() const;
+
+        void setPlace(const uint64_t &place);
+
+        bool getDead() const;
+
+        void setDead(const bool &dead);
+
+    private:
+        const int _uid;
+        uint64_t _score{}, _survivalTime{}, _place{};
+        bool _dead{};
+    };
+}
