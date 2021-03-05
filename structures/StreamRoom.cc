2341ad0d2e69e21bb5680a2ea72cf1fdd306d360
Particle_G <particle_g@outlook.com>
2021-03-06 05:43:17 +0800

    - Implement StreamRoom structure.

---
+++ structures/StreamRoom.cc
@@ -0,0 +1,92 @@
+//
+// Created by Parti on 2021/2/27.
+//
+
+#include <structures/StreamRoom.h>
+#include <utils/Utils.h>
+
+using namespace drogon;
+using namespace tech::structures;
+using namespace tech::utils;
+using namespace std;
+
+StreamRoom::StreamRoom(
+        string rid,
+        const uint64_t &capacity
+) : BaseRoom(move(rid), capacity) {
+    _innerPlace = 0;
+}
+
+void StreamRoom::publish(Json::Value &&message) {
+    {
+        shared_lock<shared_mutex> lock(_sharedMutex);
+        for (auto &pair : _connectionsMap) {
+            pair.second->send(WebSocket::fromJson(message));
+        }
+    }
+}
+
+void StreamRoom::publish(Json::Value &&message, const uint64_t &excluded) {
+    {
+        shared_lock<shared_mutex> lock(_sharedMutex);
+        for (auto &pair : _connectionsMap) {
+            if (excluded != pair.second->getContext<Stream>()->getSidsMap()->at(_id)) {
+                pair.second->send(WebSocket::fromJson(message));
+            }
+        }
+    }
+}
+
+Json::Value StreamRoom::parseInfo() const {
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    Json::Value info;
+    info["rid"] = _id;
+    info["count"] = _count;
+    info["capacity"] = _capacity;
+    return info;
+}
+
+Json::Value StreamRoom::getPlayers() const {
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    Json::Value result(Json::arrayValue);
+    for (auto &pair : _connectionsMap) {
+        Json::Value tempInfo;
+        tempInfo["uid"] = pair.second->getContext<Stream>()->getUid();
+        result.append(tempInfo);
+    }
+    return result;
+}
+
+bool StreamRoom::checkFinished() const {
+    bool finished = true;
+    {
+        shared_lock<shared_mutex> lock(_sharedMutex);
+        for (auto &pair : _connectionsMap) {
+            if (!pair.second->getContext<Stream>()->getDead()) {
+                finished = false;
+                break;
+            }
+        }
+    }
+    return finished;
+}
+
+Json::Value StreamRoom::getDeaths() const {
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    Json::Value result(Json::arrayValue);
+    for (auto &pair : _connectionsMap) {
+        auto stream = pair.second->getContext<Stream>();
+        Json::Value tempInfo;
+        tempInfo["uid"] = stream->getUid();
+        tempInfo["place"] = stream->getPlace();
+        tempInfo["score"] = stream->getScore();
+        tempInfo["survivalTime"] = stream->getSurvivalTime();
+        result.append(tempInfo);
+    }
+    return result;
+}
+
+uint64_t StreamRoom::generatePlace() {
+    unique_lock<shared_mutex> lock(_sharedMutex);
+    return ++_innerPlace;
+}
---
+++ structures/StreamRoom.h
@@ -0,0 +1,33 @@
+//
+// Created by Parti on 2021/2/27.
+//
+
+#include <structures/BaseRoom.h>
+#include <structures/Stream.h>
+
+namespace tech::structures {
+    class StreamRoom : public BaseRoom {
+    public:
+        explicit StreamRoom(
+                std::string rid,
+                const uint64_t &capacity
+        );
+
+        void publish(Json::Value &&message);
+
+        void publish(Json::Value &&message, const uint64_t &excluded);
+
+        Json::Value parseInfo() const;
+
+        Json::Value getPlayers() const;
+
+        bool checkFinished() const;
+
+        Json::Value getDeaths() const;
+
+        uint64_t generatePlace();
+
+    private:
+        uint64_t _innerPlace;
+    };
+}
