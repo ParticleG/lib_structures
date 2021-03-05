7e5a30b630d50492b0673d7ea6e3dd45fc6d800d
Particle_G <particle_g@outlook.com>
2021-03-06 05:41:45 +0800

    - Add members _config and _ready.

--- structures/Play.cc
+++ structures/Play.cc
@@ -9,7 +9,7 @@ using namespace drogon_model;
 using namespace tech::structures;
 using namespace std;
 
-Play::Play(const int64_t &id) {
+Play::Play(const int64_t &id) : BasePlayer(true) {
     Mapper<Techmino::Info> infoMapper(app().getDbClient());
     auto info = infoMapper.findOne(Criteria(Techmino::Info::Cols::__id, CompareOperator::EQ, id));
     _info = make_shared<Techmino::Info>(info);
@@ -17,4 +17,20 @@ Play::Play(const int64_t &id) {
 
 shared_ptr<Techmino::Info> Play::getInfo() const {
     return _info;
+}
+
+string Play::getConfig() const {
+    return _config;
+}
+
+void Play::setConfig(string &&config) {
+    _config = move(config);
+}
+
+bool Play::getReady() const {
+    return _ready;
+}
+
+void Play::setReady(const bool ready) {
+    _ready = ready;
 }
--- structures/Play.h
+++ structures/Play.h
@@ -10,13 +10,21 @@
 namespace tech::structures {
     class Play : public BasePlayer {
     public:
-        Play() = default;
-
-        Play(const int64_t &id);
+        explicit Play(const int64_t &id);
 
         std::shared_ptr<drogon_model::Techmino::Info> getInfo() const;
 
+        std::string getConfig() const;
+
+        void setConfig(std::string &&config);
+
+        bool getReady() const;
+
+        void setReady(const bool ready);
+
     private:
         std::shared_ptr<drogon_model::Techmino::Info> _info{};
+        std::string _config{};
+        bool _ready{};
     };
 }
