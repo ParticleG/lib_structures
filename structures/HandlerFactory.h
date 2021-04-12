6db8d27d6e7bd53a338104189b549759ec302149
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2021-04-12 22:36:20 +0800

    fix handler factory

--- structures/HandlerFactory.h
+++ structures/HandlerFactory.h
@@ -10,64 +10,34 @@
 
 namespace tech::structures {
     template<class baseHandler>
-    class HandlerRegistrarInterface {
+    class HandlerFactory final {
     public:
-        virtual baseHandler *createHandler() = 0;
+        HandlerFactory() {}
 
-    protected:
-        HandlerRegistrarInterface() {}
+        ~HandlerFactory() {}
 
-        virtual ~HandlerRegistrarInterface() {};
-    };
-
-    template<class baseHandler>
-    class HandlerFactory {
-    public:
         HandlerFactory(const HandlerFactory &) = delete;
 
         const HandlerFactory &operator=(const HandlerFactory &) = delete;
 
-        static HandlerFactory<baseHandler> &instance() {
-            static HandlerFactory<baseHandler> instance;
-            return instance;
-        };
-
-        void registerHandler(const unsigned int &action, HandlerRegistrarInterface<baseHandler> *registrar) {
+        template <class Handler>
+        void registerHandler(const unsigned int &action) {
             std::unique_lock<std::shared_mutex> lock(_sharedMutex);
-            _handlerRegistrarsMap[action] = registrar;
+            _handlerRegistrarsMap[action] = std::make_unique<Handler>();
         }
 
-        baseHandler *getHandler(const unsigned int &action) {
+        baseHandler &getHandler(const unsigned int &action) {
             std::shared_lock<std::shared_mutex> lock(_sharedMutex);
             auto iter = _handlerRegistrarsMap.find(action);
             if (iter != _handlerRegistrarsMap.end()) {
-                return iter->second->createHandler();
+                return *iter->second;
             }
             throw std::out_of_range("Action not found");
         }
 
     private:
-        HandlerFactory() {}
-
-        ~HandlerFactory() {}
-
         mutable std::shared_mutex _sharedMutex;
 
-        std::unordered_map<unsigned int, HandlerRegistrarInterface<baseHandler> *> _handlerRegistrarsMap;
-    };
-
-    template<class baseHandler, class implementedHandler>
-    class HandlerRegistrar : public HandlerRegistrarInterface<baseHandler> {
-    public:
-        explicit HandlerRegistrar(const unsigned int &action) {
-            HandlerFactory<baseHandler>::instance().registerHandler(
-                    action,
-                    this
-            );
-        }
-
-        baseHandler *createHandler() {
-            return new implementedHandler();
-        }
+        std::unordered_map<unsigned int, std::unique_ptr<baseHandler>> _handlerRegistrarsMap;
     };
 }
